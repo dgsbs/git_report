51c9fc185e71460e5ebf2c9c6a56949c7a298113
Bartosz Lesner <bsle@dgs.com>
2018-07-23 09:54:31 +0200

    1)namespace changed to GitReport.CLI. 2)Object names changed to more descriptive. 3)Public fields changed to public properties.

---
+++ src/GitReportCL/GitReport.cs
@@ -0,0 +1,25 @@
+﻿namespace GitReport.CLI
+{
+    class GitReport
+    {
+        public static GitReportHandler RunGitReport
+        {
+            get;
+        } = new GitReportHandler();
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                RunGitReport.RunGitDiffProcess.ValidateArgsForGitDiff.GetArgsForGitDiff
+                    = new GitReportArguments(args);
+                RunGitReport.RunGitReportHandler();
+            }
+            else
+            {
+                RunGitReport.RunGitDiffProcess.ValidateArgsForGitDiff.GetArgsForGitDiff
+                    = new GitReportArguments();
+                RunGitReport.RunGitReportHandler();
+            }
+        }
+    }
+}
