d8647bb7570de6fb1581c8df67ebae502bf0ff80
Bartosz Lesner <bsle@dgs.com>
2018-07-23 09:56:30 +0200

    1)namespace changed to GitReport.CLI. 2)Class name changed to GitReportArguments. 3)Public fields changed to public properties.

---
+++ src/GitReportCL/GitReportArguments.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+namespace GitReport.CLI
+{
+    class GitReportArguments
+    {
+        public string[] GitArguments
+        {
+            get;
+            set;
+        }
+        public DateTime dateSince;
+        public DateTime dateBefore;
+        public CultureInfo CultureInfo
+        {
+            get;
+            set;
+        }
+        public GitReportArguments()
+        {
+            this.GitArguments = new string[3];               
+            this.Initialization();
+        }
+        public GitReportArguments(string[] args)
+        {
+            this.GitArguments = args;
+            this.Initialization();
+        }
+        private void Initialization()
+        {
+            dateSince = new DateTime();
+            dateBefore = new DateTime();
+            CultureInfo = new CultureInfo("en-Us");
+            Thread.CurrentThread.CurrentCulture = CultureInfo;
+        }
+    }
+}
