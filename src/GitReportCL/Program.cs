fe50338b1ff5ce1493c56139b9b3b1b92ba1ef1f
Bartosz Lesner <bsle@dgs.com>
2018-07-26 14:03:11 +0200

    File and class name changed to Program. Logic changed to run simplified app

--- src/GitReportCL/Program.cs
+++ src/GitReportCL/Program.cs
@@ -1,19 +1,35 @@
-﻿namespace GitReportCL
+﻿using System;
+namespace GitReport.CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            RunGitReport report = new RunGitReport();
+            GitReportArguments getArg = new GitReportArguments();
+            DateAndPathValidation startValidation =  new DateAndPathValidation();
+            GitDiffReportProcess runProcess = new GitDiffReportProcess();
+            GitReportErrorsHandler operate = new GitReportErrorsHandler();
+            
             if (args.Length == 3)
             {
-                report.run.validate.setGitRep = new SetGitReport(args);
-                report.Report();
+                while (!startValidation.ValidateGitDiffReportArgs(args, getArg))
+                {
+                    string[] editedArgs = new string[3];
+                    operate.HandleDateFormatOrPathError(args, getArg, out editedArgs);
+                    args = editedArgs;
+                }
+                Console.WriteLine(runProcess.RunGitDiff(getArg)); 
             }
             else
             {
-                report.run.validate.setGitRep = new SetGitReport();
-                report.Report();
+                string[] newArgs = operate.CreateArgsForGitDiffReport();
+                while (!startValidation.ValidateGitDiffReportArgs(newArgs, getArg))
+                {
+                    string[] editedArgs = new string[3];
+                    operate.HandleDateFormatOrPathError(newArgs, getArg, out editedArgs);
+                    newArgs = editedArgs;
+                }
+                Console.WriteLine(runProcess.RunGitDiff(getArg));
             }
         }
     }
