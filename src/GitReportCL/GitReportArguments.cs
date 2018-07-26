65e0c2e2b02e8e530b9f4c1fa8f983b78736c617
Bartosz Lesner <bsle@dgs.com>
2018-07-26 13:52:25 +0200

    All unnecessary arguments removed

--- src/GitReportCL/GitReportArguments.cs
+++ src/GitReportCL/GitReportArguments.cs
@@ -1,38 +1,10 @@
 ﻿using System;
-using System.Globalization;
-using System.Threading;
 namespace GitReport.CLI
 {
     class GitReportArguments
     {
-        public string[] GitArguments
-        {
-            get;
-            set;
-        }
-        public DateTime dateSince;
-        public DateTime dateBefore;
-        public CultureInfo CultureInfo
-        {
-            get;
-            set;
-        }
-        public GitReportArguments()
-        {
-            this.GitArguments = new string[3];               
-            this.Initialization();
-        }
-        public GitReportArguments(string[] args)
-        {
-            this.GitArguments = args;
-            this.Initialization();
-        }
-        private void Initialization()
-        {
-            dateSince = new DateTime();
-            dateBefore = new DateTime();
-            CultureInfo = new CultureInfo("en-Us");
-            Thread.CurrentThread.CurrentCulture = CultureInfo;
-        }
+        public string GitPath { get; set;}
+        public DateTime DateSince { get; set;}
+        public DateTime DateBefore { get; set;}
     }
 }
