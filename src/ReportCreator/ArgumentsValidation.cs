7d6c1e152455ade64ad9a8a9ce2966d4b22563dc
Bartosz Lesner <bsle@dgs.com>
2018-08-28 14:09:04 +0200

    File added 2

--- src/ReportCreator/ArgumentsValidation.cs
+++ src/ReportCreator/ArgumentsValidation.cs
@@ -1,10 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ReportCreator
+namespace GitCounter
 {
-    class ArgumentsValidation
+    public class ArgumentsValidation 
     {
+        GitLogArguments gitArgument = new GitLogArguments();
+        IDirectoryValidation directoryValidation;
+        public ArgumentsValidation(GitLogArguments gitArgument,
+            IDirectoryValidation directoryValidation)
+        {
+            this.gitArgument = gitArgument;
+            this.directoryValidation = directoryValidation;
+        }
+        public bool AreDatesPathValid(string[] arguments)
+        {
+            bool sinceDateValidator = DateTime.TryParse(arguments[0], out var SinceDate);
+            gitArgument.DateSince = SinceDate;
+
+            bool beforeDateValidator = DateTime.TryParse(arguments[1], out var BeforeDate);
+            gitArgument.DateBefore = BeforeDate;
+
+            bool pathExistenceValidator =
+                directoryValidation.CheckIfDirectoryIsValid(arguments[2]);
+
+            if (pathExistenceValidator)
+            {
+                gitArgument.GitPath = arguments[2];
+            }
+            else
+            {
+                gitArgument.GitPath = "";
+            }
+
+            if (!sinceDateValidator || !beforeDateValidator ||
+                !pathExistenceValidator || SinceDate > BeforeDate)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
